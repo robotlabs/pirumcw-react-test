46f5a3f6c0056e926440e11de3be69997a99a625
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2019-10-06 12:08:54 +0100

    added redux store

---
+++ src/app/redux/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {};
+const test = (state = initialState, action) => {
+  switch (action.type) {
+    case 'TEST':
+      return state
+    
+  }
+}
+
+export default test
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import './index.css';
 import App from './app/app';
+import rootReducer from './app/redux/reducers'
 
 
 render(
