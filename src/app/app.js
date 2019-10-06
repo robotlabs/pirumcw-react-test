53dc4aa372afd8bb48d0df63ab525bcbba3d1322
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2019-10-06 12:05:28 +0100

    init (CRA) structure. render CIAO BELLA

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        CIAO BELLA
+      </div>
+    );
+  }
+}
+
+export default App;
