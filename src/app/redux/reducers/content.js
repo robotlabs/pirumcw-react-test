47218729b032f15ec796db56ffb14316bc98da78
Roberto Cascavilla <roberto.cascavilla@gmail.com>
2019-10-06 13:10:05 +0100

    toggle album reducer

--- src/app/redux/reducers/content.js
+++ src/app/redux/reducers/content.js
@@ -1,4 +1,4 @@
-import {ADD_DATA} from '../constants/action-types'
+import {ADD_DATA, TOGGLE_ALBUM} from '../constants/action-types'
 const initialState = [];
 const addData = (state = initialState, action) => {
   switch (action.type) {
@@ -6,6 +6,10 @@ const addData = (state = initialState, action) => {
       return [
         ...action.data
       ]
+    case TOGGLE_ALBUM:
+      return state.map(album => {
+          return album.id === action.id ? { ...album, opened: !album.opened } : album
+        })
     default:
       return state
   }
